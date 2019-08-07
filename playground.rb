32e46b9ba9013b7918f05626a088f3067f7bb610
harishtadikam <42519522+harishtadikam@users.noreply.github.com>
2019-08-07 16:58:22 +0530

    Add files via upload

---
+++ playground.rb
@@ -0,0 +1,9 @@
+# puts "Hello World"
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+
+end
+
+
+say_hello("Hello World From Method")
---
+++ strings.rb
@@ -0,0 +1,9 @@
+# String Concat
+# Single quote we cannot interpolate
+first_name = "Harish"
+last_name = "Tadikamalla"
+puts first_name + " " + last_name
+
+#String interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
+puts first_name.class
